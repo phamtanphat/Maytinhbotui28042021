4e8e025a31fb856db75c790d5e50becf6c121c74
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-12 21:20:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/maytinhbotui28042021/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.maytinhbotui28042021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ctrl + / : comment code
+//         In dữ liệu trong hệ thống
+//         ctrl + p : xem kiểu dữ liệu truyền vào cho hàm
+//        int a = 5;
+//        Log.d("BBB",String.valueOf(a));
+//        Toast.makeText(MainActivity.this,"123",Toast.LENGTH_LONG).show();
+
+
+    }
+}
