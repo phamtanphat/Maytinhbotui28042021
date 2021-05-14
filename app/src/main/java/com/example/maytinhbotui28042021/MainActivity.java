ccc8677822b7bbc4e1fe04ffa6ccda52cb9127cd
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-14 21:23:42 +0700

    bai tap ve nha

--- app/src/main/java/com/example/maytinhbotui28042021/MainActivity.java
+++ app/src/main/java/com/example/maytinhbotui28042021/MainActivity.java
@@ -1,25 +1,63 @@
 package com.example.maytinhbotui28042021;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText mEdtSoThu1,mEdtSoThu2;
+    TextView mTvKetQua;
+    Button mBtnCong,mBtnTru,mBtnNhan,mBtnChia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        ctrl + / : comment code
-//         In dữ liệu trong hệ thống
-//         ctrl + p : xem kiểu dữ liệu truyền vào cho hàm
-//        int a = 5;
-//        Log.d("BBB",String.valueOf(a));
-//        Toast.makeText(MainActivity.this,"123",Toast.LENGTH_LONG).show();
+        // chuẩn style code google
+
+        // Ánh xạ
+        mEdtSoThu1 = findViewById(R.id.ediTextSoThu1);
+        mEdtSoThu2 = findViewById(R.id.ediTextSoThu2);
+        mTvKetQua = findViewById(R.id.textViewKetQua);
+        mBtnCong = findViewById(R.id.buttonCong);
+        mBtnTru = findViewById(R.id.buttonTru);
+        mBtnNhan = findViewById(R.id.buttonNhan);
+        mBtnChia = findViewById(R.id.buttonChia);
+
+        //nút cộng
+        // lấy giá trị từ 2 edittext
+        // thực hiện việc cộng 2 giá trị
+        // in giá trị cho textview để hiển thị
+
+        // Task
+        // Thứ 1 : Xử lý cho các phép toán còn lại
+        // Thứ 2 : Nếu chia cho 0 thì thông báo không chia được
+        // Thứ 3 : Nếu người dùng không nhập giá trị thì thông báo
+        // Sự kiện click
+        // ctrl + p : xem kiểu dữ liệu
+        mBtnCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Lấy dữ liệu từ edittext;
+                String textSoThu1 = mEdtSoThu1.getText().toString();
+                String textSoThu2 = mEdtSoThu2.getText().toString();
+
+                int valueSoThu1 = Integer.parseInt(textSoThu1);
+                int valueSoThu2 = Integer.parseInt(textSoThu2);
 
 
+                int ketQua = valueSoThu1 + valueSoThu2;
+
+                mTvKetQua.setText("Kết quả = " + ketQua);
+            }
+        });
     }
 }
